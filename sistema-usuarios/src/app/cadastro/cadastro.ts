7b1af294c99aa855f3df75a21afdc0bddb83be80
ADS 36134652025.1M <36134652025.1M@senacrjedu.local>
2026-03-20 11:53:57 -0300

    Subindo aula dia 20

--- sistema-usuarios/src/app/cadastro/cadastro.ts
+++ sistema-usuarios/src/app/cadastro/cadastro.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule,FormsModule, FormControl, FormGroup, Validators } from  '@angular/forms';
+import { EnderecoService } from '../services/endereco.service';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule,FormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
+
+
 export class Cadastro {
+  private enderecoService=inject(EnderecoService);
+
+  nomeRegex=/^[A-Za-zÀ-ÿ]+(?:\s[A-Za-zÀ-ÿ]+)+$/;
+
+  passwordRegex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._-])[A-Za-z\d@$!%*?&._-]{8,}$/;
+
+  emailRegex=/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+form= new FormGroup({
+nome: new FormControl ('', [Validators.required,Validators.pattern(this.nomeRegex)]), //campo referente nome
+email:new FormControl ('', [Validators.required,Validators.pattern(this.emailRegex)]), //campo referente email
+senha:  new FormControl ('', [Validators.required ,Validators.pattern(this.passwordRegex)]), //campo referente senha
+cep: new FormControl(''),
+logradouro: new FormControl (''),
+
+})
+
+onSubmit () {
+
+if (this.form.valid)
+  console.log  ("Formulario valido: ", this.form.value );
+else
+  console.log("Fomulario invalido:",  this.form.value);
+
+
+}
 regraMinimoCaractere: Boolean = false;
 regraMaximocaractere: Boolean = false;
 regraLetraMaiuscula: Boolean=false;
@@ -31,6 +60,15 @@ validarSenha ( senha:string):void{
   this.regraLetraMaiuscula=rxMaiuscula.test(senha);
   this.regraCaractereEspecial=rxEspecial.test(senha);
   this.regraNumerosLetras=rxNumero.test(senha) && rxLetra.test(senha);
+}
+
+get buscarCep (){
+ const cep = this.form.get('cep')?.value ??'';
+ this.enderecoService.getEndereco(cep).subscribe(endereco=>{
+  this.form.get('logradouro')?.setValue(endereco.logradouro);
+ })
+
+ return true;
 }
 
 
---
+++ sistema-usuarios/src/app/models/endereco.model.ts
@@ -0,0 +1,9 @@
+export interface Endereco{
+
+    cep: string,
+    logradouro: string,
+    bairro:string,
+    localidade:string,
+    uf:string,
+
+}
